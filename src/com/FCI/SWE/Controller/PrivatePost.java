9d5f30195f0a9d1705aa38cd9082c92fe439823f
ibrahimali123 <ibrahim.ali.0403@gmail.com>
2015-04-19 17:09:33 +0200

    Adding Bridge Design Pattern

---
+++ src/com/FCI/SWE/Controller/PrivatePost.java
@@ -0,0 +1,21 @@
+package com.FCI.SWE.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.core.Context;
+
+//@Path("/")
+//@Produces("text/html")
+public class PrivatePost extends CreatePost{
+
+//	@POST
+//	@Path("/createpost_private")
+//	@Produces(MediaType.TEXT_PLAIN)
+	public void create(PostCreator p , @Context HttpServletRequest req , @FormParam("current_user_id") String current_user_id ,
+			@FormParam("text") String text , @FormParam("privatee") String privatee ,
+			@FormParam("publice") String publice) {
+		p.create(req, current_user_id, text, privatee, publice);
+		
+	}
+
+}
